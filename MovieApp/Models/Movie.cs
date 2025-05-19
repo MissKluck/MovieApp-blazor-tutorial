9649dad959066559a46050eb293e7bcf84790ae8
MissKluck <sigrid.b.kluck@gmail.com>
2025-05-19 10:36:45 +0200

    updated with required fields + constructor

--- MovieApp/Models/Movie.cs
+++ MovieApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp.model;
 
@@ -7,17 +8,34 @@ public partial class Movie
 {
     public int MovieID { get; set; }
 
+    [Required]
     public string Title { get; set; } = null!;
 
+    [Required]
     public string Overview { get; set; } = null!;
 
+    [Required]
     public string Genre { get; set; } = null!;
 
+    [Required]
     public string Language { get; set; } = null!;
 
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "This field accepts only positive numbers.")]
     public int Duration { get; set; }
 
+    [Required]
+    [Range(0, 10.0, ErrorMessage = "The value should be less than or equal to 10.")]
     public decimal? Rating { get; set; }
 
+    // Optional property
     public string? Posterpath { get; set; }
+
+    public Movie()
+    {
+        Title = string.Empty;
+        Overview = string.Empty;
+        Genre = string.Empty;
+        Language = string.Empty;
+    }
 }
